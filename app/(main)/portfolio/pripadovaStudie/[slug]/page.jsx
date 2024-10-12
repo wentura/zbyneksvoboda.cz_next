0e84c7fde85de014e253fe9d062887fc85883cea
Zbynek Svoboda <wentura17@seznam.cz>
2024-10-12 18:20:28 +0200

    route @

--- app/(main)/portfolio/pripadovaStudie/[slug]/page.jsx
+++ app/(main)/portfolio/pripadovaStudie/[slug]/page.jsx
@@ -1,5 +1,5 @@
+import { portfolioData } from "@/app/components/portfolioData";
 import React from "react";
-import { portfolioData } from "../../../components/portfolioData";
 export default function PripadovaStudie({ params }) {
   const item = portfolioData.find((item) => item.slug === params.slug);
 
