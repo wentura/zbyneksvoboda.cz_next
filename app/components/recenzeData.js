579e0d5bc29bd3e9ee70e24b6bab36a3bad5a292
Zbynek Svoboda <wentura17@seznam.cz>
2024-10-25 20:53:13 +0200

    nove fotky
    fotky beru z cloudinary

--- app/components/recenzeData.js
+++ app/components/recenzeData.js
@@ -5,7 +5,8 @@ export const recenzeData = [
     short: 1,
     textShort: "Výborná spolupráce v tom pravém smyslu...",
     text: "Výborná spolupráce v tom pravém smyslu - pochopení požadavků + návrh + konzultace + realizace = spokojenost. To vše rychle a operativně, DĚKUJI!!!",
-    fotka: "/recenze/js.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882223/zbyneksvoboda/portfolio2/recenze/js.webp",
   },
   {
     kdo: "Natalie Štípková",
@@ -14,7 +15,8 @@ export const recenzeData = [
     textShort:
       "Jsme velmi spokojeni s prací pana Zbyňka Svobody, který pracoval na našich webových stránkách....",
     text: "Jsme velmi spokojeni s prací pana Zbyňka Svobody, který pracoval na našich webových stránkách.<br />Jeho profesionální přístup a znalosti v oblasti webdesignu nás nadchly. Stránky, které pro nás vytvořil, jsou nejen vizuálně atraktivní, ale také velmi přehledné a snadno použitelné.<br />Zbyněk nám poskytl cenné rady ohledně rozmístění obsahu a navigace, což nám pomohlo při budování efektivního online prostoru pro kontakt s našimi klienty.<br />Celkově jsme s jeho tvorbou více než spokojeni a bez váhání mu udělujeme hodnocení pěti hvězdiček. Děkujeme!",
-    fotka: "/recenze/pu.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882225/zbyneksvoboda/portfolio2/recenze/pu.webp",
   },
   {
     kdo: "Markéta Svobodová",
@@ -22,7 +24,8 @@ export const recenzeData = [
     short: 0,
     // textShort: "Dobrý den Zbyňku, mám na webovku moc pěkné reakce...",
     text: "Dobrý den Zbyňku, mám na webovku moc pěkné reakce.<br />I od naší omladiny - synů a dcery!<br />A to je opravdu úspěch! :-)<br />Díky moc.",
-    fotka: "/recenze/ms.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882224/zbyneksvoboda/portfolio2/recenze/ms.webp",
   },
   {
     kdo: "Kristýna Franková",
@@ -30,7 +33,8 @@ export const recenzeData = [
     short: 1,
     textShort: "Spolupráci s panem Svobodou vřele doporučuji...",
     text: "Spolupráci s panem Svobodou vřele doporučuji,<br />veškerá má přání ohledně úprav designu byla obratem splněna.<br />Oceňuji rychlost a kvalitu odvedené práce a přátelskou a rychlou komunikaci.<br />Vše bylo jasně a srozumitelně vysvětleno a společně jsme pozvedli design eshopu o několik levelů výš a zlepšili některé funkce.",
-    fotka: "/recenze/frankova.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882224/zbyneksvoboda/portfolio2/recenze/ms.webp",
   },
   {
     kdo: "Jan Bláha",
@@ -39,20 +43,23 @@ export const recenzeData = [
     textShort:
       "Vážený pane Svobodo, ještě jednou děkujeme za perfektně odvedenou práci při úpravě webu...",
     text: "Vážený pane Svobodo, ještě jednou děkujeme za perfektně odvedenou práci při úpravě webu kolem-krku.cz.<br />Vše proběhlo přesně podle domluvy a velmi rychle. Web vypadá perfektně.<br />Mockrát děkujeme za Vaši práci a ochotu.<br />Moc rádi Vás doporučíme dále!",
-    fotka: "/recenze/blaha.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882223/zbyneksvoboda/portfolio2/recenze/blaha.webp",
   },
   {
     kdo: "Petr Kolář",
     co: "Doomentia.com",
     short: 0,
     text: "Zbyňku, díky moc. Super práce.<br />Zůstaňme v kontaktu na další práce.",
-    fotka: "/recenze/doom.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882223/zbyneksvoboda/portfolio2/recenze/doom.webp",
   },
   {
     kdo: "Štěpán Malý",
     co: "AntonieEmma.cz, Zoe8.eu",
     short: 0,
     text: "Super, naprostá m*da!",
-    fotka: "/recenze/maly.webp",
+    fotka:
+      "https://res.cloudinary.com/dam7wdzvx/image/upload/v1729882223/zbyneksvoboda/portfolio2/recenze/doom.webp",
   },
 ];
