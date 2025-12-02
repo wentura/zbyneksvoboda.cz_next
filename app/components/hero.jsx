acff0e6f42efd4ac53c45dc30b2f9961c9a2b3e9
wentura <svoboda.zbynek@gmail.com>
2025-12-02 21:25:11 +0100

    lespi hero na mobilu

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -10,16 +10,17 @@ export default function Hero() {
       <div className="container max-w-screen-xl mx-auto px-4 md:px-6">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-12 lg:gap-18 items-center">
           {/* Left: Text content */}
-          <div className="flex flex-col col-span-1 md:col-span-2 lg:col-span-1">
-            <h1 className="nadpisPage mb-6 md:mb-8">
+          <div className="flex flex-col col-span-1 md:col-span-2 lg:col-span-1 min-h-[83vh] md:min-h-0 justify-between md:justify-start">
+            <h1 className="nadpisPage mb-6 md:mb-8 text-5xl md:text-4xl">
               Weby, které mají strategii, strukturu a přinášejí výsledky.
             </h1>
-            <p className="text-lg md:text-xl mb-8 text-gray-700 tracking-tight">
+            <p className="text-lg md:text-xl mb-8 text-gray-700 tracking-tight hidden md:block">
               Jsem Zbyněk Svoboda,<br />webový konzultant, digitální stratég a vývojář.</p>
-              <p className="text-lg md:text-xl mb-8 text-gray-700 tracking-tight">
+              
+              <div><p className="text-2xl md:text-lg mb-8 text-gray-700 tracking-tight">
               Pomáhám firmám mít web, který není jen vizitka - ale funkční nástroj pro získávání klientů.
             </p>
-            <ul className="space-y-1.5 md:space-y-2 mb-8 text-base md:text-lg text-gray-700 tracking-tight">
+            <ul className="space-y-1.5 md:space-y-2 mb-8 text-base text-gray-700 tracking-tight">
               <li className="flex items-start">
                 <span className="text-red-400 mr-2">•</span>
                 <span>od nápadu po měřitelný výsledek</span>
@@ -32,7 +33,7 @@ export default function Hero() {
                 <span className="text-red-400 mr-2">•</span>
                 <span>spolupráce s menšími a středními firmami, které chtějí růst</span>
               </li>
-            </ul>
+            </ul></div>
             <div className="flex flex-col">
               <button
                 onClick={() => setModalOpen(true)}
